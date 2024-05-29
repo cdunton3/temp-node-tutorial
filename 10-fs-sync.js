c639b1ee38ef142ff5ee3054bd539cc155e1c821
Cory Dunton <corydunton@Corys-MacBook-Pro.local>
2024-05-28 23:08:54 -0400

    second_commit

--- 10-fs-sync.js
+++ 10-fs-sync.js
@@ -8,7 +8,7 @@ console.log(`start`)
 const first = readFileSync('./content/first.txt', 'utf8')
 const second = readFileSync('./content/second.txt', 'utf8')
 
-console.log(first,second)
+// console.log(first,second)
 
 writeFileSync(
     './content/result-sync.txt', 
